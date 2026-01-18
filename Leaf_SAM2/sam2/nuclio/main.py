f8b9eb64e7f4aa47250c285b9ab93dd64619809e
hungtran <hungtran20041210@gmail.com>
2026-01-18 18:07:55 +0700

    Sửa quy trình, thêm yolo phát hiện lá

--- Leaf_SAM2/sam2/nuclio/main.py
+++ Leaf_SAM2/sam2/nuclio/main.py
@@ -1,6 +1,3 @@
-# Copyright (C) CVAT.ai Corporation
-#
-# SPDX-License-Identifier: MIT
 import json
 import base64
 from PIL import Image
@@ -11,7 +8,6 @@ import cv2
 from ultralytics import YOLO
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
-from skimage.feature import peak_local_max
 
 def init_context(context):
     context.logger.info("Init context...  0%")
@@ -24,11 +20,13 @@ def init_context(context):
     )
     predictor = SAM2ImagePredictor(quantized_model)
     context.user_data.sam2 = predictor
-    model = YOLO("/opt/nuclio/sam2/yolo.pt")
-    context.user_data.yolo = model
+    model1 = YOLO("/opt/nuclio/sam2/yolo1.pt")
+    context.user_data.yolo1 = model1
+    model2 = YOLO("/opt/nuclio/sam2/yolo2.pt")
+    context.user_data.yolo2 = model2
     context.logger.info("Init context...100%")
 
-def check_green(image,min_green = np.array([35,50,50]),max_green = np.array([70,255,255])) :
+def check_green(image,min_green = np.array([35,20,20]),max_green = np.array([70,255,255])) :
     # Chuyển ảnh sang HSV
     hsv = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
 
@@ -38,31 +36,6 @@ def check_green(image,min_green = np.array([35,50,50]),max_green = np.array([70,
     # Xóa nhiễu và làm mịn
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
     return mask
-
-def find_leaf_points(mask,min_area = 300,max_area = 10000):
-    points = []
-    # Co ảnh
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-    erosion = cv2.erode(mask,kernel,iterations=1)
-
-    # Tìm từng vùng connected component
-    num_labels, labels,stats,centroids = cv2.connectedComponentsWithStats(erosion, connectivity=8)
-    for i in range(1,num_labels) :
-        _,_,_,_,area = stats[i]
-        if area < min_area:
-            erosion[labels == i] = 0
-        if area > min_area and area < max_area :
-            points.append(centroids[i])
-            erosion[labels == i] = 0
-
-    # Tạo bản đồ khoảng cách và tìm đỉnh
-    dist = cv2.distanceTransform(erosion, cv2.DIST_L2, 5)
-    coords = peak_local_max(dist, min_distance=30,threshold_abs=10)
-    coords = np.array([(int(x), int(y)) for (y, x) in coords])
-    for i in coords :
-        points.append(i)
-
-    return np.array(points)
 
 def find_crop(image,results) :
     boxes = results[0].boxes.xyxy
@@ -88,45 +61,7 @@ def mask_full(mask,h,w,position) :
     mask_f[y1:y2, x1:x2] = mask
     return mask_f
 
-def sam2_inputs(points, min_dist=100):
-    if len(points) == 0: return []
-    if len(points) == 1:
-        return np.array([points[0]]),np.array([1])
-
-    points = np.array(points)
-    num_points = len(points)
-    input_coords = []
-    input_labels = []
-
-    # Lấy điểm gần nhất > min dist làm negative point
-    for i in range(num_points):
-        point = points[i]
-
-        min_d_point = None
-        min_d = float('inf')
-
-        for j in range(num_points):
-            if i == j: continue
-
-            dist = np.linalg.norm(point - points[j])
-
-            if dist < min_d:
-                min_d = dist
-                min_d_point = points[j]
-
-        if min_d_point is not None and min_d > min_dist:
-            input_coord = [point,min_d_point]
-            input_label = [1,0]
-        else :
-                input_coord = [point,point]
-                input_label = [1,1]
-
-        input_coords.append(np.array(input_coord))
-        input_labels.append(np.array(input_label))
-
-    return np.array(input_coords),np.array(input_labels)
-
-def check_mask(green, s_mask, min_green_ratio=0.5):
+def check_mask(green, s_mask, min_green_ratio=0.3):
     s_mask = (s_mask > 0).astype(np.uint8) * 255
 
     # Vùng xanh của mask
@@ -141,11 +76,60 @@ def check_mask(green, s_mask, min_green_ratio=0.5):
     ratio = green_area / area_mask
 
     # Lọc theo tỉ lệ và diện tích
-    return ratio >= min_green_ratio and area_mask < 30000
+    return ratio >= min_green_ratio
 
-def change(point) :
-    points = np.array([np.array([i]) for i in point])
-    return points
+def sam2_input(yolo_results,green):
+    if len(yolo_results) == 0 or len(yolo_results[0].boxes) == 0:
+        return None, None, None
+
+    # Lấy box
+    boxes = yolo_results[0].boxes.xyxy.cpu().numpy()
+
+    # Tính tâm các box
+    center_x = (boxes[:, 0] + boxes[:, 2]) / 2
+    center_y = (boxes[:, 1] + boxes[:, 3]) / 2
+    centers = np.stack([center_x, center_y], axis=1)
+
+    # point và label prompt
+    points = centers[:, np.newaxis, :]
+    # Lấy giá trị pixel tại các điểm tâm
+    h, w = green.shape[:2]
+    cx = np.clip(center_x.astype(int), 0, w-1)
+    cy = np.clip(center_y.astype(int), 0, h-1)
+
+    is_positive = green[cy, cx] > 0
+    labels = is_positive.astype(np.float32)[:, np.newaxis]
+
+    return boxes, points, labels
+
+def clean_mask(masks, scores,thresh=0.5):
+    scores = scores.flatten()
+    if masks.ndim == 4:
+        masks = masks.squeeze(1)
+
+    # sắp xếp  theo scores
+    sorted_indices = np.argsort(scores)[::-1]
+    h, w = masks[0].shape[:2]
+    occupied_region = np.zeros((h, w), dtype=bool)
+
+    final_masks = []
+    final_indices = []
+
+    # Những vùng trùng sẽ tính theo mask score cao hơn
+    for idx in sorted_indices:
+        current_mask = masks[idx].astype(bool)
+        current_area = np.count_nonzero(current_mask)
+        cleaned_mask = current_mask & (~occupied_region)
+        cleaned_area = np.count_nonzero(cleaned_mask)
+        ratio = cleaned_area / current_area
+
+        if ratio > thresh:
+            mask_final = cleaned_mask.astype(np.uint8)
+            final_masks.append(mask_final)
+            final_indices.append(idx)
+            occupied_region = occupied_region | cleaned_mask
+
+    return final_masks, final_indices
 
 def handler(context, event):
     context.logger.info("call handler")
@@ -154,9 +138,10 @@ def handler(context, event):
     image = Image.open(buf)
     image = image.convert("RGB")  #  to make sure image comes in RGB
 
-    model = context.user_data.yolo
+    model1 = context.user_data.yolo1
+    model2 = context.user_data.yolo2
     predictor = context.user_data.sam2
-    crop_results = model.predict(image, save=False, iou=0.3)
+    crop_results = model1.predict(image, save=False)
 
     image = np.array(image)
     h,w = image.shape[:2]
@@ -166,43 +151,37 @@ def handler(context, event):
     results = []
 
     for i in range(n) :
-        green = check_green(crops[i])
         crop = crops[i]
-        hc,wc = crop.shape[:2]
-        predictor.set_image(crops[i])
-        for a in range(3) :
-            points = find_leaf_points(green)
-            if (len(points) == 0) : break
-            input_points,input_labels = sam2_inputs(points)
-            masks, scores, _ = predictor.predict(
-                point_coords=input_points,
-                point_labels=input_labels,
-                multimask_output=False,
-            )
-            mask_0 = np.zeros((hc, wc), dtype=np.uint8)
-            for mask, score in zip(masks, scores):
-                mask = np.array(mask.cpu() if hasattr(mask, "cpu") else mask)
-                mask = np.squeeze(mask).astype(np.uint8)
-                if (check_mask(green,mask) == 0) : continue
-                m = mask
-                mask = mask_full(mask,h,w,positions[i])
+        results2 = model2.predict(crop, save=False)
+        green = check_green(crop)
+        predictor.set_image(crop)
+        boxes, points, labels = sam2_input(results2,green)
+        if boxes is None: continue
+        masks, scores, _ = predictor.predict(
+            point_coords=points,
+            point_labels=labels,
+            box=boxes,
+            multimask_output=False,
+        )
 
-                # Tìm contour (polygon)
-                contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
-                if len(contours) == 0:
-                    continue
-                contour = max(contours, key=cv2.contourArea)
-                polygon = contour.flatten().tolist()
-                results.append({
-                    "confidence": str(float(score)),
-                    "label": "Spinacia",
-                    "points": polygon,
-                    "type": "polygon",
-                    })
-                mask_0[m > 0] = 1
-            if cv2.countNonZero(mask_0) == 0 : break
-            green[mask_0 > 0] = 0
+        new_masks, index = clean_mask(masks, scores)
+        for mask, idx in zip(new_masks, index):
+            mask = np.array(mask.cpu() if hasattr(mask, "cpu") else mask)
+            mask = np.squeeze(mask).astype(np.uint8)
+            if (check_mask(green,mask) == 0) : continue
+            mask = mask_full(mask,h,w,positions[i])
 
-
+            # Tìm contour (polygon)
+            contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours = [cv2.approxPolyDP(contour, epsilon=0.1, closed=True) for contour in contours]
+            if len(contours) == 0:
+                continue
+            contour = max(contours, key=cv2.contourArea)
+            polygon = contour.flatten().tolist()
+            results.append({
+                "confidence": str(float(scores[idx])),
+                "label": "Spinacia",
+                "points": polygon,
+                "type": "polygon",
+                })
     return context.Response(body=json.dumps(results), headers={},content_type='application/json', status_code=200)
--- README.md
+++ README.md
@@ -1,34 +1,46 @@
 # Leaf_SAM2
 
-## Phương pháp 
-Quy trình xử lý bao gồm :
-1. Dùng YOLOv8 detect bounding box của từng cây rau trong ảnh lớn.
-2. Dùng xử lý ảnh tìm point prompt từ các box.
-3. DÙng point prompt để SAM2 tạo mask.
-4. Lọc mask hợp lệ và ghép về ảnh gốc.
+## Quy trình xử lý
+1. **Phát hiện cây** : Dùng Yolo1 phát hiện crop chứa cây rau trong ảnh.
+2. **Phát hiện lá** : Dùng Yolo2 phát hiện box chứa lá cây trong cây rau.
+3. **Phân đoạn** : Lấy box và tâm của box làm prompt cho SAM2 tạo masks
+4. **Hậu xủ lý** : Lọc mask hợp lệ, làm sạch và ghép về ảnh gốc
+
+*Yolo1 : mô hình yolo11n phát hiện cây rau trong ảnh
+
+*Yolo2 : mô hình yolo11n phát hiện lá rau trong 1 cây 
 
 ## Triển khai 
 1. Cài đặt CVAT :
-https://github.com/cvat-ai/cvat
+
+    Làm theo hướng dẫn của cvat :
+
+    - [doc cvat](https://docs.cvat.ai/docs/getting_started/)
+
+    - [github cvat](https://github.com/cvat-ai/cvat)
 
 2. Triển khai model :
 
-    Copy [sam2/nuclio](https://github.com/hungtran1210/Leaf_SAM2/tree/main/Leaf_SAM2/sam2/nuclio) vào phần serverless của CVAT
+    Copy [sam2/nuclio](https://github.com/hungtran1210/Leaf_SAM2/tree/main/Leaf_SAM2/sam2/nuclio) vào phần [serverless](https://docs.cvat.ai/docs/getting_started) của CVAT
 
-    Chạy model bằng lệnh ./serverless/deploy_cpu.sh ./serverless/sam2
+    Chạy model bằng lệnh :
+
+    ```bash
+    docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d
+    ```
+
+    ```bash
+    ./serverless/deploy_cpu.sh serverless/sam2
+    ```
 
 ## Notebooks demo
 
-1. Minh họa phương pháp : [leaf](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/Leaf.ipynb)
+1. Minh họa phương pháp : [Leaf](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/Leaf.ipynb)
 
-2. Train yolo : [yolov8](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/Yolo8.ipynb)
+2. Train yolo : [Yolo11](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/Yolo11.ipynb), [Data](https://drive.google.com/drive/folders/1B_m5oU3yYRJjc7jnCyMX2h_7MrT6lsIv?usp=drive_link), [Data_leaf](https://drive.google.com/drive/folders/1H9VW21gfvMESuRjuQ8j7Z4iDanRXGBPR?usp=drive_link) 
 
-3. Kết quả : [test](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/test.ipynb)
+3. Kết quả : [Test](https://github.com/hungtran1210/Leaf_SAM2/blob/main/Leaf_SAM2/test.ipynb)
 
 ## Hình ảnh minh họa 
-![alt text](image.png)
-![alt text](image-1.png)
-
-
-
-
+![alt text](image1.png)
+![alt text](image2.png)
